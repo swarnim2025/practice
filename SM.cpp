883b44f304d76a9f16fcfffcf6648f3f13709c43
swarnim11023 <168832727+swarnim11023@users.noreply.github.com>
2025-01-27 01:00:02 +0530

    Create SM.cpp

---
+++ SM.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+#define ld  long double 
+
+ld f_x(ld x) {
+    ld res = 2.0*(x * x * x) - 2.5*x -5.0;
+    // ld res=-x*x+25;
+    return res;
+}
+
+
+
+int main(){
+
+ld x0=1.0;
+ld x1=2.0;
+ld epsilon=0.0001;
+ld  x2=x1-((f_x(x1)*(x1-x0))/(f_x(x1)-f_x(x0)));
+
+
+ld curr_epsilon=fabs(x2-x1)/x2;
+
+
+    cout << setprecision(15) << fixed;
+
+    // Print the table header
+    cout << setw(10) << "Iter"
+         << setw(20) << "x0"
+         << setw(20) << "x1"
+         << setw(20) << "x2"
+         << setw(20) << "Epsilon"
+         << setw(20) << "f(x2)" << endl;
+
+int maxiter=100;
+int curr_it=0;
+while(curr_epsilon>epsilon && curr_it<maxiter){
+cout << setw(10) << curr_it
+         << setw(20) << x0
+         << setw(20) << x1
+         << setw(20) << x2
+         << setw(20) << curr_epsilon
+         << setw(20) << f_x(x2 )<< endl;
+
+
+x0=x1;
+x1=x2;
+x2=x1-((f_x(x1)*(x1-x0))/(f_x(x1)-f_x(x0)));
+curr_epsilon=fabs(x2-x1)/x2;
+curr_it++;
+
+}
+cout << setw(10) << curr_it
+         << setw(20) << x0
+         << setw(20) << x1
+         << setw(20) << x2
+         << setw(20) << curr_epsilon
+         << setw(20) << f_x(x2 )<< endl;
+
+
+
+
+
+
+}
